0d0427c363781f625215a32d3668db87c398e3c0
unknown <nina.heuzer@student.vinci.be>
2020-10-27 13:51:35 +0100

    add - BerlinClockKataTest.php

---
+++ BerlinClockKataTest.php
@@ -0,0 +1,11 @@
+<?php
+require "vendor/autoload.php";
+require "BerlinClockKata.php";
+
+use BerlinClockKata\BerlinClockKata;
+use PHPUnit\Framework\TestCase;
+
+class BerlinClockKataTest extends TestCase
+{
+
+}
